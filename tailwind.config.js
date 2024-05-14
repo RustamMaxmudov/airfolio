316a35a5e2eccf8c048425e4062a1ba0b59c1f5d
Rustam <154827168+RustamMaxmudov@users.noreply.github.com>
2024-05-14 16:25:09 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-inter)", "sans-serif"],
+      },
+      colors: {
+        primary: "#1C1C57",
+        green: "#078D84",
+        blue: "#255BF9",
+        purple: "#AB29A1",
+        secondary: "#3E3E70",
+      },
+    },
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
+  },
+  plugins: [],
+};
